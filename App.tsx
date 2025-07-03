4ece1b978a66043b9e1242249e815c617654e342
SaudeBio <saudebio.com@gmail.com>
2025-07-03 22:43:39 +0100

    Update App.tsxccc

--- App.tsx
+++ App.tsx
@@ -1,29 +1,10 @@
 import { useEffect, useState } from "react";
 import { supabase } from "./supabaseClient";
 import LoginRegisto from "./LoginRegisto";
-import PlataformaSaudeBioFormacoes from "./src/components/PlataformaSaudeBioFormacoes";
+import PlataformaSaudeBioFormacoes from "./PlataformaSaudeBioFormacoes";
 
 export default function App() {
-  const [logado, setLogado] = useState(false);
-
-  useEffect(() => {
-    async function registar() {
-      const { data, error } = await supabase.from("users").insert([
-        {
-          email: "teste@exemplo.com",
-          ip: "123.123.123.123",
-          consultas_restantes: 3,
-        },
-      ]);
-      if (error) {
-        console.error("❌ Erro ao inserir:", error.message);
-      } else {
-        console.log("✅ Inserido com sucesso:", data);
-      }
-    }
-
-    registar();
-  }, []);
+  const [logado, setLogado] = useState(!!localStorage.getItem("utilizador"));
 
   return logado ? (
     <PlataformaSaudeBioFormacoes />
