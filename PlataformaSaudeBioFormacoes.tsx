70dd4e0863ad8979ccec0bbfb1c2a20b00812648
SaudeBio <saudebio.com@gmail.com>
2025-07-03 23:47:08 +0100

    Create PlataformaSaudeBioFormacoes.tsx

---
+++ PlataformaSaudeBioFormacoes.tsx
@@ -0,0 +1,8 @@
+export default function PlataformaSaudeBioFormacoes() {
+  return (
+    <div>
+      <h1>Bem-vindo à Plataforma Saúde BioFormações</h1>
+      <p>Conteúdo ainda em construção...</p>
+    </div>
+  );
+}
