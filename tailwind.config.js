331e4b5cc1b1bca8edcb1ecb75cb04925cf416fe
SaudeBio <saudebio.com@gmail.com>
2025-07-05 16:49:56 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./**/*.{ts,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
