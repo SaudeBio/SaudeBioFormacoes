c53e860f7fed7ba7820d28bf43871d4e35a71c17
SaudeBio <saudebio.com@gmail.com>
2025-07-05 16:12:13 +0100

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css"; // << ESTE IMPORT É O MAIS IMPORTANTE
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
-)
+);
