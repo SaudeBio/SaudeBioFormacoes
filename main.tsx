d6beb12bbba30ce729c69580dd6bb78426493533
SaudeBio <saudebio.com@gmail.com>
2025-07-05 17:13:09 +0100

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import "./index.css"; // << ESTE IMPORT É O MAIS IMPORTANTE
+import "./index.css"; // Importa os estilos do Tailwind CSS
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
